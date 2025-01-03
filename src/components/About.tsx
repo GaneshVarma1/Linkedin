4c81ae38b1d279a7b547d454e477f9f9e61c72b9
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-01-03 11:49:04 -0500

    Update About.tsx
    About Updated

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { User } from 'lucide-react';
-import SectionHeader from './common/SectionHeader';
+import React from "react";
+import { User } from "lucide-react";
+import SectionHeader from "./common/SectionHeader";
 
 export default function About() {
   return (
@@ -8,13 +8,24 @@ export default function About() {
       <SectionHeader icon={User} title="About" />
       <div className="prose dark:prose-invert text-gray-700 dark:text-gray-300">
         <p>
-          Full Stack Developer with 4+ years of experience building dynamic websites and web applications using HTML, CSS, JavaScript, and frameworks like React.js, Next.js, and Django. Proficient in creating seamless e-commerce platforms, integrating Stripe payment gateways, and implementing advanced SEO strategies.
+          Full Stack Developer with 4+ years of experience building dynamic
+          websites and web applications using HTML, CSS, JavaScript, and
+          frameworks like React.js, Next.js, and Django. Proficient in creating
+          seamless e-commerce platforms, integrating Stripe payment gateways,
+          and implementing advanced SEO strategies.
         </p>
         <ul className="mt-4 space-y-2">
-          <li>🚀 Leading development teams in delivering high-impact projects</li>
-          <li>💡 Expertise in React, Node.js, and cloud technologies</li>
+          <li>
+            🚀 Leading development teams in delivering high-impact projects
+          </li>
+          <li>
+            💡 Expertise in React, Next.js,TailwindCSS and cloud technologies
+          </li>
           <li>📚 Regular speaker at tech conferences and workshops</li>
-          <li>🌱 Committed to continuous learning and staying current with industry trends</li>
+          <li>
+            🌱 Committed to continuous learning and staying current with
+            industry trends
+          </li>
         </ul>
       </div>
     </div>
